fbab30cf367f51617610f0e5b693d80c56e95354
superFatDu <superduchengwu@163.com>
2019-03-20 11:51:02 +0800

    扁平化Array.

--- demos/Sorting排序/new Set().js
+++ demos/Sorting排序/new Set().js
@@ -20,4 +20,14 @@ console.log(arrResult);
 
 let multiArr = [[1, 45], 2, 777, [9, 234234, [23423, [234, 122]]]];
 let multiArrResult = Array.from(new Set(multiArr.flat(Infinity))).sort((a, b) => a -b);
-console.log(multiArrResult);
+console.log(multiArrResult); // [ 1, 2, 9, 45, 122, 234, 777, 23423, 234234 ]
+
+// 2. 问题
+// flat()函数是一个实验室函数，存在兼容性问题，可以在Array的原型上添加方法。
+
+// 3. new Set()去重
+let arrSet = new Set(arr);
+console.log(arrSet);  // Set { 4, 790, 12, 1, 3, 1000, 67, 600 }
+
+// 4. 格式成数组 => Array.from
+console.log(Array.from(arrSet));  // [ 4, 790, 12, 1, 3, 1000, 67, 600 ]
